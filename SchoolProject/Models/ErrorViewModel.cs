d2f9905d6549ce153992c613e56a204a4ffdf38c
Hasan Bolu <hasan.bolu@outlook.com>
2018-12-20 21:53:52 +0300

    Add project files.

---
+++ SchoolProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
