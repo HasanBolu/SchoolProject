1d4246d68884e3e4019733aeead904906d5e1bd8
Hasan Bolu <hasanbolu.ieee@gmail.com>
2019-09-15 03:31:58 +0300

    Update SchoolProjectContext.cs

--- SchoolProject/Models/SchoolDbContext/SchoolProjectContext.cs
+++ SchoolProject/Models/SchoolDbContext/SchoolProjectContext.cs
@@ -22,11 +22,7 @@ namespace SchoolProject.Models.SchoolDbContext
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.;Database=SchoolProject;Trusted_Connection=True;");
-            }
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
